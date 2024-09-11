40fb36795efdc50d27118c07551ce66855a9b289
BuddhadebKoner <jeetkoner36@gmail.com>
2024-09-11 20:48:04 +0530

    adding update todo functionality

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeTodo } from '../features/Todo/todoSlice';
-
+import { removeTodo, updateTodo } from '../features/Todo/todoSlice';
 
 const Todos = () => {
    const dispatch = useDispatch();
    const todos = useSelector((state) => state.todos);
+   const [editingId, setEditingId] = useState(null);
+   const [newText, setNewText] = useState('');
+
+   const handeleEditTodo = (id, currentText) => {
+      setEditingId(id);
+      setNewText(currentText);
+   };
+
+   const saveEditTodo = (id) => {
+      dispatch(updateTodo({ id, text: newText }));
+      setEditingId(null);
+      setNewText('');
+   };
 
    return (
       <div className="w-full max-w-lg mx-auto mt-6">
          {
             todos.map((todo) => (
                <div key={todo.id} className="flex items-center justify-between p-4 bg-transparent border border-gray-200 rounded-lg mt-4 text-red-50">
-                  <p>{todo.text}</p>
-                  <button onClick={() => dispatch(removeTodo(todo.id))} className="text-red-500 font-semibold">Delete</button>
+                  {
+                     editingId === todo.id ? (
+                        <input
+                           type="text"
+                           value={newText}
+                           onChange={(e) => setNewText(e.target.value)}
+                           className="text-black"
+                        />
+                     ) : (
+                        <p>{todo.text}</p>
+                     )
+                  }
+                  <div className='flex items-center space-x-4'>
+                     {
+                        editingId === todo.id ? (
+                           <button onClick={() => saveEditTodo(todo.id)} className="text-green-500 font-semibold">Save</button>
+                        ) : (
+                           <button onClick={() => handeleEditTodo(todo.id, todo.text)} className="text-blue-500 font-semibold">Edit</button>
+                        )
+                     }
+                     <button onClick={() => dispatch(removeTodo(todo.id))} className="text-red-500 font-semibold">Delete</button>
+                  </div>
                </div>
             ))
          }
